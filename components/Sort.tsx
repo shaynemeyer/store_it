61df38dba6dd11e86973d91dc4f93b4ea32e1064
Shayne Meyer <shayne.meyer@gmail.com>
2024-11-14 12:48:07 -0800

    implemented the sort

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,6 +1,40 @@
 "use client";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { sortTypes } from "@/contants";
+import { usePathname, useRouter } from "next/navigation";
+
 function Sort() {
-  return <div>Sort</div>;
+  const path = usePathname();
+  const router = useRouter();
+
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+
+  return (
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((sort) => (
+          <SelectItem
+            key={sort.label}
+            value={sort.value}
+            className="shad-select-item"
+          >
+            {sort.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 }
 export default Sort;
